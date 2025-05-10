5c9b543238759ff825899a902bcb91ed13fd22e0
JhonataCarvalho23 <jhoncsilva28v@gmail.com>
2025-05-09 21:14:23 -0300

    3.py

---
+++ 3.py
@@ -0,0 +1,11 @@
+numero = int(input('Digite um numero:'))
+
+if numero < 0:
+    print('Esse número é negativo')
+    
+elif numero > 0:
+    print('Esse número é positivo')
+    
+else:
+    print('Esse número é neutro')
+  
