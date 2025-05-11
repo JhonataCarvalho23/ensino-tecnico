2d16f8e2d72195ca59d7a5b03947b02c7e2b1044
JhonataCarvalho23 <jhoncsilva28v@gmail.com>
2025-05-11 14:22:46 -0300

    4.py

---
+++ 4.py
@@ -0,0 +1,7 @@
+idade = int(input('Fale sua idade:'))
+
+if idade >= 18:
+    print('Você pode assistir o filme')
+    
+else:
+    print('Você não pode assistir o filme, pois você é menor de idade')
